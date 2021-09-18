cddb789ae94ae0fac14080b6a67ac9c69d606d35
Caique Alves <calves@mundolivre.digital>
2021-09-18 12:52:32 -0300

    manipulando hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 function App() {
-  const [ativo, setAtivo] = React.useState(false) 
+  const [ativo, setAtivo] = useState(false) 
+  const [contar, setContar] = useState(0) 
+  const [pessoa, setPessoa] = useState({nome:'',idade:0}) 
+
   console.log('valor',ativo,
-  '\nfuncao:',setAtivo) 
+  '\nContar:',contar,
+  '\nPessoa:',pessoa) 
   return (
     <>
+    <div>
+    <p>mudar o stado do botão</p>
     <button onClick={()=>setAtivo(!ativo)}>{ativo?"Ativo":"Desativado"}</button>
+    </div>
+    <div>
+      <p>Somatória com react hook</p>
+      <button onClick={()=>setContar(contar+1)}>{contar}</button>
+    </div>
+    <div>
+      <form>
+        <lable>Nome</lable>
+        <input onChange={(e)=>setPessoa({...pessoa,nome:e.target.value})}/>
+        <lable>Idade</lable>
+        <input type="number" onChange={(e)=>setPessoa({
+         ...pessoa,idade:e.target.value 
+        })}/>
+      </form>
+    </div>
+    <br/>
+    <p>{pessoa.nome}</p>
+    <p>{pessoa.idade}</p>
     </>
   );
 }
