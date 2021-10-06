da95ead4a33380b2b9d17349911730ca901edf5d
Caique Alves <calves@mundolivre.digital>
2021-10-05 21:54:18 -0300

    trabalhando ciclo de vida do useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React, { useState } from 'react'
-import Home from './components/Home/Home'
-function App() {
+import react from 'react'
+import React from 'react'
+const App = () => {
 
-  const [contador, setContado] = useState(1)
-  const [items, setItems] = useState(['item1'])
+  //ciclos de vida hooks useEffect
 
-  function handleClick() {
-    setContado((contador) => {
+  // ciclos de vida 
+
+  // momento  que ele é renderizado na tela primeiramente
+
+  // momento que ele é alterado (atualizado)
+
+  //momento que ele some da tela
+
+  const [contar, setCount] = React.useState(0)
+  const [data, setData] = React.useState(null)
+  console.log('execultou fora')
+  React.useEffect(() => {
+
+    console.log('execultou')
+  }, []) //se eu colocar um array ele vai execultar somente uma vez
+
+  //podemos ter varios efeitos no nosso componente
+
+  React.useEffect(() => { // efeito que sla muda o titulo do site
+
+    document.title = " Mudando " + contar
+  }, [contar])//colocado um dependencia
+
+  // React.useEffect(()=>{ // JEITO ERRADO POIS O STADO VAI MUDAR MAIS O TITLE DO TITULO NÃO
+
+  //   document.title=" Mudando "+contar
+  // },[ ]) 
 
 
-      return contador + 1
-    })
-    setItems(() => [...items, `items${contador + 1}`])
+  //nesse exemplo iremos chamar um evento para carregar somente uma vez alguns dados vindo de uma api(ou poderia ser servidor ...)
+
+  React.useEffect(async () => {
+    console.log(data)
+    try {
+      const response = await fetch(" https://ranekapi.origamid.dev/json/api/produto/notebook")
+      const data = await response.json()
+      setData(data)
+
+    } catch (error) {
+      setData('erro')
+    }
+    return()=>{ // se caso o useEffect tiver algum return significa que quando o cara desmontar o componente 
+      //ele sempre vai ser execultado sempre usar uma função
+      
+      setData()
+      console.log('data',data)
+    }
+  }, [])
+
+  const renderApi=()=> {
+
+    if (data!==null && data!=="erro") {
+      return (
+        <>
+        <div>
+          Produto:{data.nome}
+        </div>
+        <div>
+          Preço:{data.preco*contar}
+        </div>
+        </>
+      )
+      }else if(data==="erro"){
+        return(
+          <>
+          erro na requisição
+          </>
+        )
+      }
   }
- 
+
   return (
     <>
-    <Home/> 
-    </> 
-  );
+      {renderApi()}
+      <button onClick={() => setCount(contar + 1)}>{contar}</button>
+    </>
+  )
 }
 
 export default App;
